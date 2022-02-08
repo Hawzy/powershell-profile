d853c430f47f3d6c7d60e34d3e2e5daf50ee67d1
Hawzy <52145494+Hawzy@users.noreply.github.com>
2022-02-07 22:59:35 -0800

    add PSReadLine

--- profile.ps1
+++ profile.ps1
@@ -5,3 +5,7 @@ foreach ( $includeFile in ("azure", "defaults", "openssl", "dev", "windows") ) {
     Unblock-File $profileDir\$includeFile.ps1
 . "$profileDir\$includeFile.ps1"
 }
+
+set-poshprompt night-owl
+
+Set-PSReadLineOption -PredictionSource History
